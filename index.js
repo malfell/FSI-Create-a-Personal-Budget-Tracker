d0907d8a3fb09a3b744b52c947a09e70727769c4
unknown <malfellows13@gmail.com>
2022-02-17 19:22:14 -0600

    reversed the questions

--- index.js
+++ index.js
@@ -9,17 +9,38 @@ let annualExpenses = 0
 // let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
 // let numberAnswer = parseFloat(stringAnswer)
 
-for (let i = 0; i < weeklyExpenseQuestions.length; i++) { 
+//TESTING ABOVE-----------
+
+// for (let i = 0; i < weeklyExpenseQuestions.length; i++) { 
+//     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+//     weeklyExpenses = weeklyExpenses + answer
+// }
+
+// for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+//     let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+//     monthlyExpenses = monthlyExpenses + answer
+// }
+
+// for (let i = 0; i < annualExpenseQuestions.length; i++) {
+//     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+//     annualExpenses = annualExpenses + answer
+// }
+
+//BONUS #1 Try to ask the questions from each array in reverse order, 
+//by changing the initializer, condition, and afterthought of each 
+//for loop.
+
+for (let i = 2; (i < weeklyExpenseQuestions.length) && (i > -1); i--) { 
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 }
 
-for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+for (let i = 3; (i < monthlyExpenseQuestions.length) && (i > -1); i--) {
     let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
     monthlyExpenses = monthlyExpenses + answer
 }
 
-for (let i = 0; i < annualExpenseQuestions.length; i++) {
+for (let i = 2; (i < annualExpenseQuestions.length) && (i > -1); i--) {
     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
     annualExpenses = annualExpenses + answer
-}
+} 
